25bb584019be28119a6265e9e657d480560a64f3
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-11-15 17:21:09 +0200

    Initial commit with firebase architecture components

---
+++ src/app/pages/contatcs/contatcs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contatcs',
+  standalone: true,
+  imports: [],
+  templateUrl: './contatcs.component.html',
+  styleUrl: './contatcs.component.css'
+})
+export class ContatcsComponent {
+
+}
