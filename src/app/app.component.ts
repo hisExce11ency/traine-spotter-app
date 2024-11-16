b89a3c2640bfbd2d4e26162d5e9c4c59a9bee801
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-11-16 23:17:39 +0200

    Added few updates to the html files and css styles of the components so fara, some basic routing and resources.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from './pages/about/about.component';
+import { AuthComponent } from './auth/auth.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthComponent } from './auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'auth', component: AuthComponent },
+];
