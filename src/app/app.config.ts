ea23efd2dee5eaf5b3f5464a1d3874808af243a3
hisExce11ency <157422766+hisExce11ency@users.noreply.github.com>
2024-11-18 15:53:16 +0200

    Installed Firebase and AngularFire dependencies
    Set up Firebase in my application, configured environments, and initialised providers.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideHttpClient(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+
     provideRouter(routes)
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyBI8GNmADn_WvfEDUbFonrzxlMDNBUHXCk",
+        authDomain: "traine-spotter.firebaseapp.com",
+        projectId: "traine-spotter",
+        storageBucket: "traine-spotter.firebasestorage.app",
+        messagingSenderId: "519124030490",
+        appId: "1:519124030490:web:fe90540aecf3490a966da3"
+    }
+};
